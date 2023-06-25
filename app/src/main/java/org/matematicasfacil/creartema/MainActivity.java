5a1780fa81246313e1111e67b04b15694600e63e
hipolito <huaylla7@hotmail.com>
2023-06-25 18:56:24 -0400

    Fondo incluido

--- app/src/main/java/org/matematicasfacil/creartema/MainActivity.java
+++ app/src/main/java/org/matematicasfacil/creartema/MainActivity.java
@@ -1,11 +1,13 @@
 package org.matematicasfacil.creartema;
 
-import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,24 +16,51 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Toast.makeText(this, "OnCreate Actividad 1", Toast.LENGTH_SHORT).show();
+        // Actividad creada.
+
         Button boton1 = findViewById(R.id.btn1);
         Button boton2 = findViewById(R.id.btn2);
 
-        boton1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setTheme(R.style.Theme_CrearTema);
-                recreate();
-            }
+        boton1.setOnClickListener(v -> {
+            recreate();
+            setTheme(R.style.Tema1);
         });
 
-        boton2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                setTheme(R.style.Theme_CrearTema2);
-                recreate();
-            }
+        boton2.setOnClickListener(v -> {
+            setTheme(R.style.Tema2);
+            recreate();
         });
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "OnStart Actividad 1", Toast.LENGTH_SHORT).show();
+        // Actividad inicializada.
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "OnResume Actividad 1", Toast.LENGTH_SHORT).show();
+        // Actividad visible.
+    }
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "OnPause Actividad 1", Toast.LENGTH_SHORT).show();
+        // Para pausar o ajustar las operaciones que no deben continuar.
+    }
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "OnStop Actividad 1", Toast.LENGTH_SHORT).show();
+        // La actividad ya no es visible (ahora está "detenida")
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "OnDestroy Actividad 1", Toast.LENGTH_SHORT).show();
+        // La actividad está terminando (debido a que el usuario la descarta por completo o a que se llama a finish()).
+    }
 }
